f09b8359841e13320731d1a1fe33a68ce64f787a
lionardus87 <yudhi_lee@hotmail.com>
2025-02-15 10:58:48 +1100

    lab exercise 4

---
+++ labexercise4/math.js
@@ -0,0 +1,16 @@
+function plus(a,b){
+    return a + b
+}
+function minus(a,b){
+    return a - b
+}
+function divide(a,b){
+    return a / b
+}
+function multiple(a,b){
+    return a * b
+}
+console.log(plus( 1, 5))
+console.log(minus( 3, 1))
+console.log(divide( 9, 3))
+console.log(multiple( 2, 2))
