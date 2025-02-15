d2e551efa3eedd8ca74d24fe9b0a105c348326f2
lionardus87 <yudhi_lee@hotmail.com>
2025-02-15 12:56:06 +1100

    update exercise 7

--- labexercise7/test.js
+++ labexercise7/test.js
@@ -1,26 +1,51 @@
-function plus(a,b){
-    return a + b
-}
-function minus(a,b){
-    return a - b
-}
-function divide(a,b){
-    return a / b
-}
-function multiple(a,b){
-    return a * b
-}
-// console.log(plus( 1, 5))
-// console.log(minus( 3, 1))
-// console.log(divide( 9, 3))
-// console.log(multiple( 2, 2))
+const prompt = require("prompt-sync")();
 
-function testSum(){
-    if(plus(3,4) != 7){
-        return false;
-    } else {
+// Get user to input numbers
+const a = prompt("Enter First number: ");
+const b = prompt("Enter Second number: ");
+
+function plus(a, b) {
+  return a + b;
+}
+function minus(a, b) {
+  return a - b;
+}
+function divide(a, b) {
+  return b / a;
+}
+function multiple(a, b) {
+  return a * b;
+}
+
+function testSum() {
+  if (plus(4, 3) === 7 && plus(0, 5) === 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function divideZero() {
+  if (b == 0) {
+    return "Cannot divide zero";
+  } else {
+    return true;
+  }
+}
+
+function testMultiple(){
+    if(multiple(2,3) === 6 && multiple(-3,3) === -9){
         return true;
+    }else{
+        return false;
     }
 }
 
-console.log(testSum());
+console.log("Test for plus function: ", testSum());
+console.log("Test for divide function: ", divideZero());
+console.log("Test for multiple function: ", testMultiple());
+
+console.log(`Add: ${plus(a, b)}`);
+console.log(`Sub: ${minus(a, b)}`);
+console.log(`Multipe: ${multiple(a, b)}`);
+console.log(`Divide: ${divide(a, b)}`);
