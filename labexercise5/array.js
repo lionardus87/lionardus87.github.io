695541e38fb80df730999dbce77c2c234df01c8b
lionardus87 <yudhi_lee@hotmail.com>
2025-02-15 11:16:01 +1100

    changes lab exercise 5

--- labexercise5/array.js
+++ labexercise5/array.js
@@ -1,8 +1,10 @@
 const fruit = ["apple"," mango","orange","strawberry","banana"]
 console.log(fruit)
 
-// [fruit[0], fruit[3]] = [fruit[3], fruit[0]];
-// console.log(fruit)
+const temp = fruit[1];
+fruit[1] = fruit[4];
+fruit[4] = temp ;
+console.log(fruit)
 
 fruit.unshift("lime")
 console.log(fruit)
